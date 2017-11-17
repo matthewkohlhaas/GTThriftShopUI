08133adc2d7f541900be3a002aeabaf707b1dde5
mkohlhaas3 <mkohlhaas3@gatech.edu>
2017-11-17 01:59:44 -0500

    improve style of listings screen and title bar

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -3,8 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
-  styleUrls: ['./title-bar.component.css',
-    '../../../node_modules/bootstrap/dist/css/bootstrap.css']
+  styleUrls: ['./title-bar.component.css']
 })
 export class TitleBarComponent {
   constructor() {}

e6715ef32bb7d0276977ddbf219b60a5ffe2a8d2
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-01-21 23:17:30 -0500

    implement front end validation for create account

--- src/app/create-account-page/create-account-page.component.ts
+++ src/app/create-account-page/create-account-page.component.ts
@@ -1,25 +1,76 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AccountService} from '../../services/account.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {ModalContentComponent} from '../modal-content/modal-content.component';
+
+const EMAIL_REGEX = /^.+@gatech.edu$/i;
+const MIN_PASSWORD_LENGTH = 6;
 
 @Component({
   selector: 'app-create-account-page',
   templateUrl: './create-account-page.component.html',
   styleUrls: ['./create-account-page.component.css']
 })
-export class CreateAccountPageComponent {
+export class CreateAccountPageComponent implements OnInit{
 
   private firstName: string;
   private lastName: string;
   private email: string;
   private password: string;
 
+  private showErrorFirstName: boolean;
+  private showErrorLastName: boolean;
+  private showErrorEmail: boolean;
+  private showErrorPassword: boolean;
+
   constructor(private accountService: AccountService, private modalService: NgbModal) {}
+
+  private static validateEntry(entry: string, validator: (str: string) => boolean): boolean {
+    const trimmedEntry = (entry) ? entry.trim() : '';
+    return validator(trimmedEntry);
+  }
+
+  ngOnInit() {
+    this.showErrorFirstName = false;
+    this.showErrorLastName = false;
+    this.showErrorEmail = false;
+    this.showErrorPassword = false;
+  }
+
+  private onSubmit(): void {
+    this.validateAllFields();
+    if (!this.showErrorFirstName && !this.showErrorLastName && !this.showErrorEmail && !this.showErrorPassword) {
+      this.createAccount();
+    }
+  }
+
+  private validateFirstName(): void {
+    this.showErrorFirstName = CreateAccountPageComponent.validateEntry(this.firstName, str => str === '');
+  }
+
+  private validateLastName(): void {
+    this.showErrorLastName = CreateAccountPageComponent.validateEntry(this.lastName, str => str === '');
+  }
+
+  private validateEmail(): void {
+    this.showErrorEmail = CreateAccountPageComponent.validateEntry(this.email, str => !EMAIL_REGEX.test(str));
+  }
+
+  private validatePassword(): void {
+    this.showErrorPassword = CreateAccountPageComponent.validateEntry(this.password, str => {
+      return str.length < MIN_PASSWORD_LENGTH;
+    });
+  }
+
+  private validateAllFields(): void {
+    this.validateFirstName();
+    this.validateLastName();
+    this.validateEmail();
+    this.validatePassword();
+  }
 
   private createAccount(): void {
     this.accountService.createAccount(this.email, this.password, this.firstName, this.lastName, msg => {
-
         const content = this.modalService.open(ModalContentComponent);
 
         if (msg.successful) {

ef49f8c06be3befd64857840fa6645f687aa3ec8
Scott Wolfe <swolfe8@gatech.edu>
2018-02-10 09:51:50 -0500

    add admin option to nav menu

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {AccountService} from '../../services/account.service';
 import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 import {ModalContentComponent} from '../modal-content/modal-content.component';
@@ -9,12 +9,24 @@ import {ModalContentComponent} from '../modal-content/modal-content.component';
   styleUrls: ['./nav-menu.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+
+  private isAdmin = false;
 
   constructor(private accountService: AccountService, private modalService: NgbModal) { }
 
+  ngOnInit(): void {
+    // ask server if user is an admin
+  }
+
   private logout(): void {
     this.accountService.logout();
+  }
+
+  private admin(): void {
+    const content: NgbModalRef = this.modalService.open(ModalContentComponent);
+    content.componentInstance.title = 'You\'re and Admin!';
+    content.componentInstance.message = 'Look at you, being a admin.';
   }
 
   private settings(): void {

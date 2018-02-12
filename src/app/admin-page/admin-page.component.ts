0e8c4b14ae905441dd8028a7d971eb3373d9bd13
Scott Wolfe <swolfe8@gatech.edu>
2018-02-12 12:29:54 -0500

    add admin calls, modal service

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AdminService} from '../../services/admin.service';
+import {AccountService} from '../../services/account.service';
+import {ModalService} from '../../services/modal.service';
 
 @Component({
   selector: 'app-admin-page',
@@ -8,9 +11,83 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
+  private adminEmail: string;
+  private userToBanEmail: string;
+  private userToUnbanEmail: string;
+
+  private registerAdminDisabled = false;
+  private banUserDisabled = false;
+  private unbanUserDisabled = false;
+
+  constructor(private adminService: AdminService, private modalService: ModalService) { }
 
   ngOnInit() {
   }
+  private registerAdmin(): void {
+    if (!this.validateEntry(this.adminEmail)) {
+      return;
+    }
+    this.registerAdminDisabled = true;
+    this.adminService.registerAdmin(this.adminEmail)
+      .subscribe(res => {
+        this.modalService.displayModal('Successful', res.text);
+        this.registerAdminDisabled = false;
+      }, err => {
+        if (err.status === 0) {
+          this.modalService.displayModal('Unsuccessful', 'Failed to connect to server.');
+        } else {
+          this.modalService.displayModal('Unsuccessful', err.error);
+        }
+        this.registerAdminDisabled = false;
+      }
+    );
+  }
 
+  private banUser(): void {
+    if (!this.validateEntry(this.userToBanEmail)) {
+      return;
+    }
+    this.banUserDisabled = true;
+    this.adminService.banUser(this.userToBanEmail)
+      .subscribe(res => {
+          this.modalService.displayModal('Successful', res.text);
+          this.banUserDisabled = false;
+        }, err => {
+          if (err.status === 0) {
+            this.modalService.displayModal('Unsuccessful', 'Failed to connect to server.');
+          } else {
+            this.modalService.displayModal('Unsuccessful', err.error);
+          }
+          this.banUserDisabled = false;
+        }
+      );
+  }
+
+  private unbanUser(): void {
+    if (!this.validateEntry(this.userToUnbanEmail)) {
+      return;
+    }
+    this.unbanUserDisabled = true;
+    this.adminService.unbanUser(this.userToUnbanEmail)
+      .subscribe(res => {
+          this.modalService.displayModal('Successful', res.text);
+          this.unbanUserDisabled = false;
+        }, err => {
+          if (err.status === 0) {
+            this.modalService.displayModal('Unsuccessful', 'Failed to connect to server.');
+          } else {
+            this.modalService.displayModal('Unsuccessful', err.error);
+          }
+          this.unbanUserDisabled = false;
+        }
+      );
+  }
+
+  private validateEntry(entry): boolean {
+    if (AccountService.validateNotEmpty(entry)) {
+      return true;
+    }
+    this.modalService.displayModal('Unsuccessful', 'Please enter an email address.');
+    return false;
+  }
 }

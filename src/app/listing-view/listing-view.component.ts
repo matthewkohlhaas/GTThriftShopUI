f86f15c6d211c0e3f14d0f8ccd8cb1ee496c85b6
Adu Malladi <adu.malladi@yahoo.com>
2018-02-14 13:50:20 -0500

    fix percentage issue

---
+++ src/app/listing-view/listing-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-view',
+  templateUrl: './listing-view.component.html',
+  styleUrls: ['./listing-view.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ListingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

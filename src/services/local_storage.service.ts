52d54af12f827ad6e221d439aabb59e12e5bcf8c
Scott Wolfe <swolfe8@gatech.edu>
2018-02-16 22:05:24 -0500

    add is_admin local storage

--- src/services/local_storage.service.ts
+++ src/services/local_storage.service.ts
@@ -1,35 +1,55 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../environments/environment';
 
-const EXPIRATION_TIME = 30 * 60 * 1000;
-const TOKEN_EXPIRATION_MAPPING = 'TOKEN_EXPIRATION_MAPPING';
+const EXPIRATION_TIME = 30 * 60 * 1000; // 30 minutes * 60 seconds/minute * 1,000 ms/second
+const ACCESS_TOKEN = 'access_token';
+const IS_ADMIN = 'is_admin';
 
 @Injectable()
 export class LocalStorageService {
 
   constructor() {}
 
-  public addAccessToken(token): void {
+  public static addAccessToken(token): void {
+    this.addItem(token, ACCESS_TOKEN);
+  }
+
+  public static removeAccessToken(): void {
+    localStorage.removeItem(ACCESS_TOKEN);
+  }
+
+  public static getAccessToken(): string {
+    return this.getItem(ACCESS_TOKEN);
+  }
+
+  public static addIsAdmin(isAdmin): void {
+    this.addItem(isAdmin, IS_ADMIN);
+  }
+
+  public static removeIsAdmin(): void {
+    localStorage.removeItem(IS_ADMIN);
+  }
+
+  public static getIsAdmin(): string {
+    return this.getItem(IS_ADMIN);
+  }
+
+  private static addItem(item, label): void {
     const expiration = new Date().getTime() + EXPIRATION_TIME;
-    const mapping = { 'token': token, 'expiration': expiration };
-    localStorage.setItem(TOKEN_EXPIRATION_MAPPING, JSON.stringify(mapping));
+    const mapping = { label: item, 'expiration': expiration };
+    localStorage.setItem(label, JSON.stringify(mapping));
     return;
   }
 
-  public removeAccessToken(): void {
-    localStorage.removeItem(TOKEN_EXPIRATION_MAPPING);
-    return;
-  }
-
-  public getAccessToken(): string {
-    const json_mapping = localStorage.getItem(TOKEN_EXPIRATION_MAPPING);
+  private static getItem(label): string {
+    const json_mapping = localStorage.getItem(label);
     if (json_mapping) {
       const mapping = JSON.parse(json_mapping);
       const current_time = new Date().getTime();
       if (mapping['expiration'] <= current_time) {
         return null;
       }
-      return mapping['token'];
+      return mapping[label];
     }
     return null;
   }

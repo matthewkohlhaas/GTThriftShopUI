42e52963d951eebd4ea5b57870395c746d3a5d4a
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-02-26 02:47:11 -0500

    Remove unused dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {
   MatButtonModule,
   MatCardModule,
@@ -112,8 +111,7 @@ export function jwtOptionsFactory() {
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule,
-    NgbModule.forRoot()
+    MatCardModule
   ],
   providers: [
     HttpClient,

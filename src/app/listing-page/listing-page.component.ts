e1c0fc3797995508fca20e491ea963fbcaba31e9
Scott Wolfe <swolfe8@gatech.edu>
2018-03-07 15:34:43 -0500

    add dropdown for sorting

--- src/app/listing-page/listing-page.component.ts
+++ src/app/listing-page/listing-page.component.ts
@@ -5,6 +5,14 @@ import {ModalFlagListingContentComponent} from '../modal-flag-listing-content/mo
 import {FlagService} from '../../services/flag.service';
 import {ModalService} from '../../services/modal.service';
 
+const SORT_OBJECTS = [
+  { text: 'Price: Low to High', params: { sort: 'price', direction: 'ascending' } },
+  { text: 'Price: High to Low', params: { sort: 'price', direction: 'descending' } },
+  { text: 'Seller Rating', params: { sort: 'userRating', direction: 'descending' } },
+  { text: 'Date: Most Recent', params: { sort: 'createdAt', direction: 'descending' } },
+  { text: 'Date: Least Recent', params: { sort: 'createdAt', direction: 'ascending' } }
+];
+
 @Component({
   selector: 'app-listing-page',
   templateUrl: './listing-page.component.html',
@@ -14,8 +22,8 @@ export class ListingPageComponent implements OnInit {
 
   private categories: string[] = ['all'];
   private selectedCategory: string;
-  private sorts: string[] = ['price', 'date', 'rating'];
-  private selectedSort: string;
+  private sorts: Object[] = SORT_OBJECTS;
+  private selectedSort: Object;
 
   listings: Listing[];
 
@@ -27,7 +35,15 @@ export class ListingPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.selectedCategory = this.categories[0];
-    this.listingService.getListings().subscribe(res => {
+    const params = { sort: 'createdAt', direction: 'descending' };
+    this.listingService.getListings(params).subscribe(res => {
+      this.listings = res;
+    });
+  }
+
+  list(): void {
+    const params = this.buildParams();
+    this.listingService.getListings(params).subscribe(res => {
       this.listings = res;
     });
   }
@@ -36,4 +52,28 @@ export class ListingPageComponent implements OnInit {
     this.modalService.openModal<ModalFlagListingContentComponent>(ModalFlagListingContentComponent,
       {listing: listing});
   }
+
+  private buildParams(): Object {
+    const params: Object = {};
+    this.addSortParams(params);
+    this.addCategoryParams(params);
+    return params;
+  }
+
+  private addSortParams(params: Object) {
+    if (this.selectedSort) {
+      for (const key in this.selectedSort) {
+        if (this.selectedSort.hasOwnProperty(key)) {
+          params[key] = this.selectedSort[key];
+        }
+      }
+    }
+  }
+
+  private addCategoryParams(params: Object) {
+    if (this.selectedCategory) {
+      params['category'] = this.selectedCategory;
+    }
+  }
+
 }

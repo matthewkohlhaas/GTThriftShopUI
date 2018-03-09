904ef4d2d033c6068c0755d4e0d6032a7f8d0c7f
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-03-09 00:52:53 -0500

    Fix edit listing modal display bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -97,6 +97,7 @@ export function jwtOptionsFactory() {
   ],
   entryComponents: [
     ModalAlertContentComponent,
+    ModalEditListingContentComponent,
     ModalFlagListingContentComponent
   ],
   imports: [

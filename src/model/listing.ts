6b1bba76f097d84987f73e693494df0cb7faa0d0
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-03-09 00:34:47 -0500

    Update user to have id field

--- src/model/listing.ts
+++ src/model/listing.ts
@@ -1,10 +1,10 @@
 import {User} from './user';
 
 export class Listing {
+  _id: string;
   name: string;
   description: string;
   price: number;
   imageUrl: string;
   user: User;
-  _id: string;
 }

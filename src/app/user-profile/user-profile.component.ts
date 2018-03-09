bece8fd1b688e8b42854b8c4e657277c9b41840a
Courtney Pettiford <courtneypettiford@Courtneys-MacBook-Pro.local>
2018-03-08 22:20:40 -0500

    added code for checking for empty user fields

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -23,10 +23,12 @@ export class UserProfileComponent implements OnInit {
 
   private editProfileBoolean = false;
 
+
   constructor(private accountService: AccountService, private router: Router, private modalService: ModalService) {
   }
 
   ngOnInit(): void {
+
     this.accountService.getCurrentUser().subscribe(res => {
       this.user = res;
     }, err => {
@@ -42,63 +44,60 @@ export class UserProfileComponent implements OnInit {
     this.editProfileBoolean = false;
   }
 
-
   private updateFirstName(): void {
     if (!ValidationUtils.validateNotEmpty(this.firstName)) {
       return;
     }
-      this.accountService.updateFirstName(this.firstName,  msg => {
-        let title = 'Failed to update first name.';
-
-        if (msg.successful) {
-          title = 'Successfully updated first name.';
-          this.user.firstName = this.firstName;
-        }
+    this.accountService.updateFirstName(this.firstName,  msg => {
+      let title = 'Failed to update first name.';
+      if (msg.successful) {
+        title = 'Successfully updated first name.';
+        this.user.firstName = this.firstName;
+      }
         this.modalService.openAlertModal(title, msg.text);
       });
   }
 
-
   private updateLastName(): void {
-    if (ValidationUtils.validateNotEmpty(this.lastName)) {
-      this.accountService.updateLastName(this.lastName,  msg => {
-        let title = 'Failed to update last name.';
-
-        if (msg.successful) {
-          title = 'Successfully updated last name.';
-          this.user.lastName = this.lastName;
-        }
-        this.modalService.openAlertModal(title, msg.text);
-      });
+    if (!ValidationUtils.validateNotEmpty(this.lastName)) {
+      return;
     }
+    this.accountService.updateLastName(this.lastName,  msg => {
+      let title = 'Failed to update last name.';
+      if (msg.successful) {
+        title = 'Successfully updated last name.';
+        this.user.lastName = this.lastName;
+      }
+      this.modalService.openAlertModal(title, msg.text);
+      });
   }
 
   private updatePictureUrl(): void {
-    if (ValidationUtils.validateNotEmpty(this.profilePictureUrl)) {
-      this.accountService.updateProfilePictureUrl(this.profilePictureUrl,  msg => {
-        let title = 'Failed to update profile picture.';
-
-        if (msg.successful) {
-          title = 'Successfully updated profile picture.';
-          this.user.profilePictureUrl = this.profilePictureUrl;
-        }
-        this.modalService.openAlertModal(title, msg.text);
-      });
+    if (!ValidationUtils.validateNotEmpty(this.profilePictureUrl)) {
+      return;
     }
+    this.accountService.updateProfilePictureUrl(this.profilePictureUrl,  msg => {
+      let title = 'Failed to update profile picture.';
+      if (msg.successful) {
+        title = 'Successfully updated profile picture.';
+        this.user.profilePictureUrl = this.profilePictureUrl;
+      }
+      this.modalService.openAlertModal(title, msg.text);
+      });
   }
 
   private updateProfileBio() {
-    if (ValidationUtils.validateNotEmpty(this.profileBio)) {
-      this.accountService.updateProfileBio(this.profileBio,  msg => {
-        let title = 'Failed to update profile bio.';
-
-        if (msg.successful) {
-          title = 'Successfully updated profile bio.';
-          this.user.profileBio = this.profileBio;
-        }
-        this.modalService.openAlertModal(title, msg.text);
-      });
+    if (!ValidationUtils.validateNotEmpty(this.profileBio)) {
+      return;
     }
+    this.accountService.updateProfileBio(this.profileBio,  msg => {
+      let title = 'Failed to update profile bio.';
+      if (msg.successful) {
+        title = 'Successfully updated profile bio.';
+        this.user.profileBio = this.profileBio;
+      }
+      this.modalService.openAlertModal(title, msg.text);
+      });
   }
 
 }

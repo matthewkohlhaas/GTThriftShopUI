2d2d85a57ac14a88e0c941c25e604f8fabb81b14
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-03-08 19:15:34 -0500

    Fix route name for flagging listings

--- src/services/flag.service.ts
+++ src/services/flag.service.ts
@@ -12,7 +12,7 @@ export class FlagService {
   constructor(private http: HttpClient) {}
 
   flagListing(listing: Listing, description: string, next?: (msg: ServerMessage) => void): void {
-    this.http.post<ServerMessage>(environment.serverUrl + '/flag/listing-flag',
+    this.http.post<ServerMessage>(environment.serverUrl + '/flags/listing-flags',
       {description: description, listing: listing})
       .subscribe(
       res => {

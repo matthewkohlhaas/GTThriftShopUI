0ff9379741f542a3d5e47529612681a4e5420ef6
Scott Wolfe <swolfe8@gatech.edu>
2018-03-09 13:02:11 -0500

    remove unecessary import

--- src/services/listing.service.ts
+++ src/services/listing.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Listing} from '../model/listing';
 import {Observable} from 'rxjs/Observable';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
 import {ServerMessage} from '../model/server-message';
 

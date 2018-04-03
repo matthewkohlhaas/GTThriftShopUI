f8e1ceb5c2f5fae8b750e990f7f3687c535e57ca
Bang <bangpham@gatech.edu>
2018-04-03 03:30:08 -0400

    setup account settings tab and account page

---
+++ src/app/account-settings-page/account-settings-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {MatTableDataSource} from "@angular/material";
+
+@Component({
+  selector: 'app-account-settings-page',
+  templateUrl: './account-settings-page.component.html',
+  styleUrls: ['./account-settings-page.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AccountSettingsPageComponent {
+
+  displayedColumns = ['position', 'name'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+}
+
+export interface Element {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, name: 'Hydrogen'},
+  {position: 2, name: 'Helium'},
+  {position: 3, name: 'Lithium'},
+];

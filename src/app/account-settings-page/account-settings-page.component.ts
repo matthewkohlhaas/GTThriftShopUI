82ed6c0e6979b2f0e044a68bcf48a2ec694ba788
Bang <bangpham@gatech.edu>
2018-04-03 06:48:04 -0400

    implemented basic table

--- src/app/account-settings-page/account-settings-page.component.ts
+++ src/app/account-settings-page/account-settings-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {MatTableDataSource} from "@angular/material";
+import {Params, Router, ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {AccountService} from "../../services/account.service";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-account-settings-page',
@@ -7,27 +11,62 @@ import {MatTableDataSource} from "@angular/material";
   styleUrls: ['./account-settings-page.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AccountSettingsPageComponent {
 
-  displayedColumns = ['position', 'name'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class AccountSettingsPageComponent implements OnInit{
+  private  testUser : User;
+  private  test : string;
 
-  applyFilter(filterValue: string) {
+  private currentUser : User;
+  private blockedList : User[];
+  private nameList : string[]
+
+  private blockTableData : tableData[] = [];
+  private displayedColumns = ['position', 'name'];
+//  private dataSource;
+  private dataSource = new MatTableDataSource()
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private accountService: AccountService,
+  ) { }
+
+  ngOnInit(): void {
+    this.accountService.getCurrentUser().subscribe((value => {
+      this.currentUser = value;
+      this.blockedList = this.currentUser.blockedUsers;
+
+      for (let i = 0; i < this.blockedList.length; i++) {
+
+        this.userService.getUserById(this.blockedList[0].toString()).subscribe(value => {
+          //this.nameList.push(value.firstName);
+          this.testUser = value;
+          this.test = value.email;
+        });
+
+        const bang: tableData = {
+          position: i,
+          name: this.blockedList[i].toString()
+        };
+        this.blockTableData.push(bang);
+
+      }
+      this.dataSource = new MatTableDataSource(this.blockTableData);
+    }));
+
+
+  }
+
+  private applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
 }
 
-export interface Element {
-  name: string;
+export interface tableData {
   position: number;
-  weight: number;
-  symbol: string;
+  name: string;
 }
 
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen'},
-  {position: 2, name: 'Helium'},
-  {position: 3, name: 'Lithium'},
-];
+

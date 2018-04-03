136e50451a3774ad43d7bff4a18e92d299607a5a
Bang <bangpham@gatech.edu>
2018-04-03 09:46:25 -0400

    implemented basic table without mat-table

--- src/app/account-settings-page/account-settings-page.component.ts
+++ src/app/account-settings-page/account-settings-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {MatTableDataSource} from "@angular/material";
-import {Params, Router, ActivatedRoute} from "@angular/router";
 import {UserService} from "../../services/user.service";
 import {AccountService} from "../../services/account.service";
 import {User} from "../../model/user";
@@ -13,20 +11,12 @@ import {User} from "../../model/user";
 })
 
 export class AccountSettingsPageComponent implements OnInit{
-  private  testUser : User;
-  private  test : string;
 
   private currentUser : User;
-  private blockedList : User[];
-  private nameList : string[]
+  private blockedList : User[] = [];
+  private nameList : string[] = []
 
-  private blockTableData : tableData[] = [];
-  private displayedColumns = ['position', 'name'];
-//  private dataSource;
-  private dataSource = new MatTableDataSource()
   constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
     private userService: UserService,
     private accountService: AccountService,
   ) { }
@@ -37,31 +27,17 @@ export class AccountSettingsPageComponent implements OnInit{
       this.blockedList = this.currentUser.blockedUsers;
 
       for (let i = 0; i < this.blockedList.length; i++) {
-
-        this.userService.getUserById(this.blockedList[0].toString()).subscribe(value => {
-          //this.nameList.push(value.firstName);
-          this.testUser = value;
-          this.test = value.email;
+        this.userService.getUserById(this.blockedList[i].toString()).subscribe(value => {
+          let name = value.firstName.concat(' ', value.lastName);
+          this.nameList.push(name);
         });
 
-        const bang: tableData = {
-          position: i,
-          name: this.blockedList[i].toString()
-        };
-        this.blockTableData.push(bang);
-
       }
-      this.dataSource = new MatTableDataSource(this.blockTableData);
+
+
     }));
-
-
   }
 
-  private applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
 }
 
 export interface tableData {
